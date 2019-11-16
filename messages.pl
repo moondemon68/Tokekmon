29437e31caa602a78d85570f36c60c61c60c05f8
MuslimMujahid <muslimmujahid1712@gmail.com>
2019-11-16 20:05:25 +0700

    fix 1.0

--- messages.pl
+++ messages.pl
@@ -6,13 +6,6 @@ msgA :-
     write('\nGoing left 1 block...\n').
 msgD :-
     write('\nGoing right (when nothing goes wrong) 1 block...\n').
-msgFence :- 
-    write('\nyOu crashed into a fence!!!!!\n'),
-    write('You lost your dignity crashing into a fence!\n').
-msgGym :-
-    write('\nYou are now in the Gym!\n'),
-    write('You can only heal once.\n'),
-    write('Write "heal" to heal, ignore me if you don\'t want to\n').
 newline :-
     write('\n').
 animatePikachu1:-                    
